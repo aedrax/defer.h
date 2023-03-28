d8e86c4945492262f37237bc47cec62edad86405
Paul Sorensen <paulsorensen5@gmail.com>
2023-03-28 13:16:16 -0400

    Add example.c

---
+++ example.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "defer.h"
+
+int main()
+{
+    int *resource = malloc(sizeof(int));
+    *resource = 42;
+    printf("Allocated resource %d\n", *resource);
+    DEFER({
+        printf("Cleaning up resource %d\n", *resource);
+        free(resource);
+    });
+    // End of block - resource is automatically cleaned up here
+
+    // Do something with the resource
+    printf("Using resource %d\n", *resource);
+    return 0;
+}
